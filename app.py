17bcbbcd3fd1a2cb40bb4a9a4c21b448ec0af21b
Inácio Bovo <inacioitz1@gmail.com>
2026-03-23 23:07:48 -0300

    Primerio passo para o gerenciador de uniforme 2.0. Agora ultilzando flask.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from models import Uniforme
+
+app = Flask (__name__)
+
+estoque = []
+
+@app.route('/')
+def index ():
+    return render_template ('cadastro.html', estoque = estoque)
+
+@app.route ('/cadastrar', methods=['POST'])
+def cadastrar():
+    nome = request.form.get('nome')
+    tamanho = request.form.get('tamanho')
+    quantidade = int(request.form.get('quantidade'))
+    setor = request.form.get('setor')
+
+    novo_item  = Uniforme (nome, tamanho, quantidade, setor)
+    estoque.append(novo_item)
+
+    return render_template('cadastro.html', estoque=estoque, mensagem= "cadastro realizado com sucesso!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+class Uniforme:
+    nome: str
+    tamanho: str
+    quantidade: int
+    setor: str
